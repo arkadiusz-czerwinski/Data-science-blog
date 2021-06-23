1254c78619b78c7fe71af66f298f15f9be25a99c
Arkadiusz Czerwiński <42278982+arkadiusz-czerwinski@users.noreply.github.com>
2021-06-23 19:53:56 +0000

    "initial adjustments"

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   url: "https://nextjs-mdx-blog-starter.vercel.app/",
-  title: "Next.js MDX Blog Starter",
-  subtitle: "Content authoring with MDX. Powered by Next.js.", // longDescription : appears in landing page meta description
+  title: "Data Science Blog",
+  subtitle: "Data Science blog focusing on the topic of Advanced Data Science and MLOps", // longDescription : appears in landing page meta description
   description:
-    "Next.js MDX Blog Starter for building blogs with Next.js and MDX, including Theme UI Component Design System, Vercel Deployment and more.",
-  author: "John Polacek",
+    "Data Science blog focusing on the topic of Advanced Data Science and MLOps",
+  author: "Arkadiusz Czerwiński",
   analytics: "", // put your google analytics code here to add tracking
   shareImage: "https://nextjs-mdx-blog-starter.vercel.app/img/screenshot.png",
   shareImageAlt: "Next.js MDX Blog Starter Screenshot",
